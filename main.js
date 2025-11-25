9ebc09673e19db37fea35a11792e925c913bb0eb
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-11-25 12:22:20 -0600

    Add JavaScript code

---
+++ main.js
@@ -0,0 +1,15 @@
+const ratingSystem = document.getElementById('rating-system');
+const submitButton = document.getElementById('submit-button');
+const ratingOutputText = document.getElementById('rating-output-text');
+let selectedRating = 0;
+let totalRating = 0;
+let numberOfRatings = 0;
+
+submitButton.addEventListener('click', () => {
+    totalRating += selectedRating;
+    numberOfRatings++;
+});
+
+function changeRating(rating) {
+    selectedRating = rating;
+}
