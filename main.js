2f8fdb616c7ad9a48e719712b3eeacdc90b3086e
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-11-25 12:22:30 -0600

    Format JavaScript code

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const ratingSystem = document.getElementById('rating-system');
-const submitButton = document.getElementById('submit-button');
-const ratingOutputText = document.getElementById('rating-output-text');
+const ratingSystem = document.getElementById("rating-system");
+const submitButton = document.getElementById("submit-button");
+const ratingOutputText = document.getElementById("rating-output-text");
 let selectedRating = 0;
 let totalRating = 0;
 let numberOfRatings = 0;
 
-submitButton.addEventListener('click', () => {
+submitButton.addEventListener("click", () => {
     totalRating += selectedRating;
     numberOfRatings++;
 });
