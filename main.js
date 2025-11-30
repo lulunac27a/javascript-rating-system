38c30f0cfdd697eaa88fa004430c02e4aadee7ff
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-11-30 13:09:44 -0600

    Change function position

--- main.js
+++ main.js
@@ -5,14 +5,13 @@ let selectedRating = 0; //selected rating
 let totalRating = 0; //total rating
 let numberOfRatings = 0; //number of ratings
 
+function changeRating(rating) {
+    //function to change selected rating
+    selectedRating = rating; //set selected rating to the given rating
+}
 submitButton.addEventListener("click", () => {
     //when submit button is clicked
     totalRating += selectedRating; //add selected rating to total rating
     numberOfRatings++; //increase number of ratings by 1
     ratingOutputText.textContent = `Selected rating: ${selectedRating}/5\nAverage rating: ${(totalRating / numberOfRatings).toFixed(2)}\nTotal ratings: ${numberOfRatings}`; //update rating output text
 });
-
-function changeRating(rating) {
-    //function to change selected rating
-    selectedRating = rating; //set selected rating to the given rating
-}
